22de61e6d05ab9259dbdd4b7f21c99b865da413c
Nhyv <33763969+Nhyv@users.noreply.github.com>
2022-02-09 15:28:18 -0500

    Login

---
+++ app/src/main/java/com/example/beyourownbartender/data/LoginDataSource.java
@@ -0,0 +1,27 @@
+package com.example.beyourownbartender.data;
+
+import com.example.beyourownbartender.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+// We will do the database stuff here.
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}

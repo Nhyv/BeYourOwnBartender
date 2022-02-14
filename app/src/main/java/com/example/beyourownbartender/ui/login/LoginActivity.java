98dc4543a51e7de6d6fa437a5755999fd8beead0
Nhyv <33763969+Nhyv@users.noreply.github.com>
2022-02-14 08:29:03 -0500

    Login to Main

--- app/src/main/java/com/example/beyourownbartender/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/beyourownbartender/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.beyourownbartender.LoadingActivity;
+import com.example.beyourownbartender.MainActivity;
 import com.example.beyourownbartender.R;
 import com.example.beyourownbartender.ui.login.LoginViewModel;
 import com.example.beyourownbartender.ui.login.LoginViewModelFactory;
@@ -136,7 +139,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 

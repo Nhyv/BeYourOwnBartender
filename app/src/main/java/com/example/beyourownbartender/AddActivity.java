a71441de5bfc669ff3950bd761d309da7d4fc3d0
Thomas valois <73235176+tomval123@users.noreply.github.com>
2022-02-21 11:05:36 -0500

    Formulaire d'ajout de recette avec layout pour les recycler views

--- app/src/main/java/com/example/beyourownbartender/AddActivity.java
+++ app/src/main/java/com/example/beyourownbartender/AddActivity.java
@@ -1,14 +1,24 @@
 package com.example.beyourownbartender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class AddActivity extends AppCompatActivity {
+    private RecyclerView rvIngredients;
+    private RecyclerView rvSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        rvIngredients = findViewById(R.id.rvIngredients);
+        rvIngredients.setLayoutManager(new LinearLayoutManager(this));
+
+        rvSteps = findViewById(R.id.rvSteps);
+        rvSteps.setLayoutManager(new LinearLayoutManager(this));
     }
 }

a69f6d76da02a7eff966affa579821c8e3ad4c64
Nhyv <33763969+Nhyv@users.noreply.github.com>
2022-03-23 10:27:19 -0400

    Awoo

--- app/src/main/java/com/example/beyourownbartender/Startup/LoginActivity.java
+++ app/src/main/java/com/example/beyourownbartender/Startup/LoginActivity.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -62,7 +60,7 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.settingsUsername);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
-        register = findViewById(R.id.register);
+        register = findViewById(R.id.registerLogin);
 
         username.addTextChangedListener(new TextWatcher() {
             @Override
